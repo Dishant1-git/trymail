fb46da541605b7d25a0943b0c7598e768f4238dd
Dishant1-git <Dishantkaushal456@gmail.com>
2026-05-11 16:36:42 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,23 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const[mail,setmail]=useState('')
+
+
+
+const send=async()=>{
+  const result=await fetch('http://localhost:9000/api/nodemail',{
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify({email:mail})
+  })
+if(result.ok){
+  const data=await result.json()
+  if(data.statuscode===1){
+    alert("Email sent successfully!")
+  } else {
+    alert("Error occurred while sending email.")
+  }
+}
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <input type='text' value={mail} onChange={(e)=>setmail(e.target.value)}></input>
+   <br></br>
+   <button onClick={send} >send</button>
+   </>
   );
 }
 
